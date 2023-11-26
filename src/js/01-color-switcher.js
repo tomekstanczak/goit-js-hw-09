1a3dc0877983af4033ea9ca9e9601f4c4c99ecc8
tomekstanczak <tomek.stanczak87@gmail.com>
2023-11-26 20:40:36 +0100

    feat: change color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const bodyBackground = document.querySelector('body');
+let intervalSetup;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.style.cursor = 'pointer';
+stopButton.style.cursor = 'pointer';
+
+const backgroundColorStart = function () {
+  intervalSetup = setInterval(() => {
+    bodyBackground.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.setAttribute('disabled', true);
+  stopButton.removeAttribute('disabled');
+};
+
+const backgroundColorStop = function () {
+  clearInterval(intervalSetup);
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', true);
+};
+
+startButton.addEventListener('click', backgroundColorStart);
+stopButton.addEventListener('click', backgroundColorStop);
