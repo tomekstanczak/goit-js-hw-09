b208ec3060f8e3a33236bb1dc9c4633e71f2f761
tomekstanczak <135948583+tomekstanczak@users.noreply.github.com>
2023-11-26 19:41:40 +0000

    Deploying to gh-pages from @ tomekstanczak/goit-js-hw-09@1a3dc0877983af4033ea9ca9e9601f4c4c99ecc8 🚀

---
+++ 01-color-switcher.abb41ee2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o;t.style.cursor="pointer",e.style.cursor="pointer";t.addEventListener("click",(function(){o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.abb41ee2.js.map
